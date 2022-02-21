a80c79176c1c5cb9d21ca74a6138c197729500e7
justinPzk <justinparzik@earthlink.net>
2022-02-21 15:46:20 -0800

    Added audio files and added stop button

--- met.js
+++ met.js
@@ -4,6 +4,7 @@ console.log("Javascript Intialized");
 document.addEventListener("DOMContentLoaded", function(event){
 document.querySelector('#submit').addEventListener('click', e =>{
     e.preventDefault();
+    console.log("Start Button Pressed");
     let bars = document.querySelector('#bars').value;
     let min = document.querySelector('#min').value;
     let max = document.querySelector('#max').value;
@@ -12,4 +13,9 @@ document.querySelector('#submit').addEventListener('click', e =>{
     console.log(max);
 
 });
+
+document.querySelector('#stop').addEventListener('click', e =>{
+    console.log("Stop Button Pressed");
+});
+
 });
