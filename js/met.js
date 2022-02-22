23dcb8c8eeeb1db3fd447def08982903657cf330
Justin <justinparzik@earthlink.net>
2022-02-21 22:14:16 -0800

    Added Metronome Functionality
    Added functionality including the automatic changing of tempo within the specified range after the specified amount of bars have passed. Also added functionality to stop button.

--- js/met.js
+++ js/met.js
@@ -2,32 +2,66 @@ console.log("Javascript Intialized");
 const hi = new Audio('./audio/hi.wav');
 const lo = new Audio('./audio/lo.wav');
 import Timer from './timer.js';
+let count = 0;
+let min = 70;
+let max = 110;
+let bpm = 70;
+let playing = false;
+let bars = 1;
+let bar = 0;
 
 document.addEventListener("DOMContentLoaded", function(event){
 document.querySelector('#submit').addEventListener('click', e =>{
     e.preventDefault();
     console.log("Start Button Pressed");
-    let bars = document.querySelector('#bars').value;
-    let min = document.querySelector('#min').value;
-    let max = document.querySelector('#max').value;
-    console.log(bars);
-    console.log(min);
-    console.log(max);
-    if (bars == 0 || max < min || min == max) {
+    bars = document.querySelector('#bars').value;
+    min = document.querySelector('#min').value;
+    max = document.querySelector('#max').value;
+    console.log(`Bars: ${bars}`);
+    console.log(`Min: ${min}`);
+    console.log(`Max: ${max}`);
+    if (bars == 0 || min == max) {
         alert (`Bars (${bars}) must be greater than 0 and the minimum tempo (${min}) must be greater than the maximum (${max}).`);
     }
     else {
-        let bpm = Math.random() * (max - min) + min;
+        bpm = Math.floor(Math.random() * (max - min)) + parseInt(min);
+        playing = true;
+        metronome.timeInterval = 60000 / bpm;
+        metronome.start();
         console.log(bpm);
     }
 });
 
 document.querySelector('#stop').addEventListener('click', e =>{
     console.log("Stop Button Pressed");
+    metronome.stop();
+    playing = false;
 });
 
 });
 
+
+function playClick() {
+    if (count === 4) {
+        count = 0;
+        bar++;
+    }
+    if (bar == bars) {
+        bar = 0;
+        let newbpm = Math.floor(Math.random() * (max - min)) + parseInt(min);
+        metronome.timeInterval = 60000 / newbpm
+        console.log(`New Tempo ${newbpm}`)
+    }
+    if (count === 0) {
+        hi.play();
+        hi.currentTime = 0;
+    } else{
+        lo.play();
+        lo.currentTime = 0;
+    }
+
+    count++;
+}
 
 const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
 
