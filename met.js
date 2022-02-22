b1fa700626747163fb0fa7c51d5578099adedde5
justinPzk <justinparzik@earthlink.net>
2022-02-21 16:18:32 -0800

    Added handling of incorrect value input

--- met.js
+++ met.js
@@ -11,7 +11,15 @@ document.querySelector('#submit').addEventListener('click', e =>{
     console.log(bars);
     console.log(min);
     console.log(max);
-
+    if (min > max){
+        alert (`Your minimum tempo is ${min}bpm, which is greater than the maximum tempo of ${max}bpm. Please assure that the minimum tempo is less than the maximum!`);
+    }
+    else if (min = max) {
+        alert (`You entered ${min}bpm as both values. Please make the maximum tempo greater than the minimum!`)
+    }
+    else {
+        return;
+    }
 });
 
 document.querySelector('#stop').addEventListener('click', e =>{
